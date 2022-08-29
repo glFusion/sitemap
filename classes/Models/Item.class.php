6019b96a34c3b1c033979e764ec4c46f7800cb85
Lee Garner <lee@leegarner.com>
2022-08-29 11:11:18 -0700

    Add model to standardize item info layout

--- classes/Models/Item.class.php
+++ classes/Models/Item.class.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Definition for a sitemap item.
+ * Allows plugins to be sure all needed elements are provided.
  *
  * @author      Lee Garner <lee@leegarner.com>
  * @copyright   Copyright (c) 2022 Lee Garner <lee@leegarner.com>
@@ -87,6 +88,84 @@ class Item implements \ArrayAccess
     public function toArray()
     {
         return $this->properties;
+    }
+
+
+    /**
+     * Set the item ID value.
+     *
+     * @param   string  $id     Item ID
+     * @return  object  $this
+     */
+    public function withItemId(string $id) : self
+    {
+        $this->properties['id'] = $id;
+        return $this;
+    }
+
+
+    /**
+     * Set the item's parent ID value.
+     *
+     * @param   string  $id     Parent ID
+     * @return  object  $this
+     */
+    public function withParentId(string $id) : self
+    {
+        $this->properties['pid'] = $id;
+        return $this;
+    }
+
+
+    /**
+     * Set the title string.
+     *
+     * @param   string  $title  Title string
+     * @return  object  $this
+     */
+    public function withTitle(string $title) : self
+    {
+        $this->properties['title'] = $title;
+        return $this;
+    }
+
+
+    /**
+     * Set the item URL.
+     *
+     * @param   string  $url    URL to the item
+     * @return  object  $this
+     */
+    public function withUrl(string $url) : self
+    {
+        $this->properties['uri'] = $url;
+        return $this;
+    }
+
+
+    /**
+     * Set the item's update date.
+     *
+     * @param   string  $title  Title string
+     * @return  object  $this
+     */
+    public function withDate(string $date) : self
+    {
+        $this->properties['date'] = $date;
+        return $this;
+    }
+
+
+    /**
+     * Set the URL to the item's image.
+     *
+     * @param   string  $url    Image URL
+     * @return  object  $this
+     */
+    public function withImageUrl(string $url) : self
+    {
+        $this->properties['image_uri'] = $url;
+        return $this;
     }
 
 }
