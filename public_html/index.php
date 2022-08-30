9585557a666fd217915efd0e4d78043fc0091586
Lee Garner <lee@leegarner.com>
2022-08-30 08:54:16 -0700

    Capitalize Driver objects, accept NULL to indicate no plugin entrypoint

--- public_html/index.php
+++ public_html/index.php
@@ -48,7 +48,7 @@ function SITEMAP_getSelectForm($selected = 'all')
     global $_CONF, $LANG_SMAP;
 
     $this_script = $_CONF['site_url'] . '/sitemap/index.php';
-    $drivers = Sitemap\Plugin::getDrivers();
+    $Drivers = Sitemap\Plugin::getDrivers();
     $LT = new Template($_CONF['path'] . '/plugins/' . Config::PI_NAME. '/templates');
     $LT->set_file('selector', 'selector.thtml');
     $LT->set_var(array(
@@ -56,11 +56,11 @@ function SITEMAP_getSelectForm($selected = 'all')
         'all_sel'   => $selected == 'all' ? 'selected="selected"' : '',
     ) );
     $LT->set_block('selector', 'selectOpts', 'opts');
-    foreach ($drivers as $driver) {
+    foreach ($Drivers as $Driver) {
         $LT->set_var(array(
-            'driver_name'   => $driver->getName(),
-            'driver_display_name' => $driver->getDisplayName(),
-            'selected' => $selected == $driver->getName() ? 'selected="selected"' : '',
+            'driver_name'   => $Driver->getName(),
+            'driver_display_name' => $Driver->getDisplayName(),
+            'selected' => $selected == $Driver->getName() ? 'selected="selected"' : '',
         ) );
 
         $LT->parse('opts', 'selectOpts', true);
@@ -74,13 +74,13 @@ function SITEMAP_getSelectForm($selected = 'all')
 /**
 *   Builds items belonging to a category
 *
-*   @param $driver  reference to driver object
+*   @param $Driver  reference to driver object
 *   @param $pid     id of parent category, may be false
 *   @return         array of ( num_items, html )
 *
 *   @destroy        $T->var( 'items', 'item', 'item_list' )
 */
-function SITEMAP_buildItems($driver, $pid)
+function SITEMAP_buildItems($Driver, $pid)
 {
     global $_CONF, $T, $_USER;
 
@@ -95,18 +95,18 @@ function SITEMAP_buildItems($driver, $pid)
     } else {
         $sp_except = array();
     }
-    $key = $driver->getName() . '_' . $pid;
+    $key = $Driver->getName() . '_' . $pid;
     $items = Sitemap\Cache::get($key);
     if ($items === NULL) {
-        $items = $driver->getItems($pid);
-        Sitemap\Cache::set($key, $items, array($driver->getName(), 'plugin'));
+        $items = $Driver->getItems($pid);
+        Sitemap\Cache::set($key, $items, array($Driver->getName(), 'plugin'));
     }
     $num_items = count($items);
     if ($num_items > 0 && is_array($items)) {
         foreach ($items as $item) {
-            $link = COM_createLink($driver->Escape($item['title']),
+            $link = COM_createLink($Driver->Escape($item['title']),
                     $item['uri'],
-                    array('title'=> $driver->Escape($item['title'])) );
+                    array('title'=> $Driver->Escape($item['title'])) );
             $T->set_var('item', $link);
             if ($item['date'] !== false) {
                 $dt->setTimestamp($item['date']);
@@ -125,13 +125,13 @@ function SITEMAP_buildItems($driver, $pid)
 /**
  * Builds a category and items belonging to it.
  *
- * @param   object  $driver Reference to driver object
+ * @param   object  $Driver Reference to driver object
  * @param   array   $cat    Array of category data
  * @return  array       Array of (num_items, HTML string)
  *
  * @destroy        $T->var( 'child_categories', 'category', 'num_items' )
  */
-function SITEMAP_buildCategory(object $driver, array $cat) : array
+function SITEMAP_buildCategory(object $Driver, array $cat) : array
 {
     global $T, $LANG_SMAP;
 
@@ -139,18 +139,18 @@ function SITEMAP_buildCategory(object $driver, array $cat) : array
     $temp = $T->get_var('child_categories');    // Push $T->var('child_categories')
 
     // Builds {child_categories}
-    $key = $driver->getName() . '_category_' . $cat['id'];
+    $key = $Driver->getName() . '_category_' . $cat['id'];
     $child_categories = Sitemap\Cache::get($key);
     if ($child_categories === NULL) {
-        $child_categories = $driver->getChildCategories($cat['id']);
-        Sitemap\Cache::set($key, $child_categories, array($driver->getName(), 'plugin'));
+        $child_categories = $Driver->getChildCategories($cat['id']);
+        Sitemap\Cache::set($key, $child_categories, array($Driver->getName(), 'plugin'));
     }
 
     if (count($child_categories) > 0) {
         $child_cats = '';
 
         foreach ($child_categories as $child_category) {
-            list($num_child_category, $child_cat) = SITEMAP_buildCategory($driver, $child_category);
+            list($num_child_category, $child_cat) = SITEMAP_buildCategory($Driver, $child_category);
             $num_total_items += $num_child_category;
             $child_cats      .= $child_cat;
         }
@@ -167,15 +167,15 @@ function SITEMAP_buildCategory(object $driver, array $cat) : array
         // If an empty category title comes in, default to 'Uncategorized'
         $cat['title'] = $LANG_SMAP['uncategorized'];
     }
-    if ($cat['uri'] !== false) {
+    if ($cat['uri']) {
         $category_link = '<a href="' . $cat['uri'] . '">'
-              . $driver->escape($cat['title']) . '</a>';
+              . $Driver->escape($cat['title']) . '</a>';
     } else {
-        $category_link = $driver->escape($cat['title']);
+        $category_link = $Driver->escape($cat['title']);
     }
 
     // Builds {items}
-    list($num_items, $items) = SITEMAP_buildItems($driver, $cat['id']);
+    list($num_items, $items) = SITEMAP_buildItems($Driver, $cat['id']);
     $num_total_items += $num_items;
     $T->set_var('num_items', $num_items);
     if (!empty($items)) {
@@ -215,42 +215,46 @@ $T->set_file(array(
 // Used below to write the sitemap and in the selection creation above.
 // Ensures that only valid driver classfiles are used.
 
-$drivers = Sitemap\Plugin::getDrivers();
-foreach ($drivers as $driver) {
+$Drivers = Sitemap\Plugin::getDrivers();
+foreach ($Drivers as $Driver) {
     $num_items = 0;
 
     // Only display enabled selected driver, or "all"
-    if ( !$driver->html_enabled ||
-        ($selected != 'all' && $selected != $driver->getName()) ) {
+    if (
+        !$Driver->html_enabled ||
+        ($selected != 'all' && $selected != $Driver->getName())
+    ) {
         continue;
     }
 
-    $entry = $driver->getEntryPoint();
-    if ($entry === false) {
-        $entry = $driver->getDisplayName();
+    $entry = $Driver->getEntryPoint();
+    if ($entry == NULL) {   // some plugins may return false
+        $entry = $Driver->getDisplayName();
     } else {
-        $entry = '<a href="' . $entry . '">' . $driver->getDisplayName()
+        $entry = '<a href="' . $entry . '">' . $Driver->getDisplayName()
                . '</a>';
     }
     $T->set_var('lang_data_source', $entry);
 
-    $categories = $driver->getChildCategories(false);
+    $categories = $Driver->getChildCategories(false);
     if (count($categories) == 0) {
-        list($num_items, $items) = SITEMAP_buildItems($driver, false);
+        list($num_items, $items) = SITEMAP_buildItems($Driver, false);
         $T->set_var('category_list', $items);
     } else {
         $cats = '';
         foreach ($categories as $category) {
-            list($num_cat, $cat) = SITEMAP_buildCategory($driver, $category);
+            list($num_cat, $cat) = SITEMAP_buildCategory($Driver, $category);
             $cats .= $cat;
             $num_items += $num_cat;
         }
 
         $T->set_var('categories', $cats);
+        $T->set_var('pi_name', $Driver->getName());
         $T->parse('category_list', 't_category_list');
     }
     if ($num_items == 0) continue;
     $T->set_var('num_items', $num_items);
+    $T->set_var('pi_name', $Driver->getName());
     $T->parse('data_sources', 't_data_source', true);
 }
 
